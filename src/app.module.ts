ca7809dff777a22a32e4a2a3cbc26ff2a655b7e8
LeeJoungwoo <ljw8124@gmail.com>
2026-01-28 17:32:28 +0900

    NestJS 에서 TypeORM 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 // import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -15,6 +16,16 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       autoSchemaFile: true, //join(process.cwd(), 'src/schema.gql'), // true 로 생성 시 gql 파일이 자동으로 생성되지 않음
     }),
     RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'ljw8124',
+      // password: '',  // localhost 에서는 생략해도 무방함
+      database: 'KoffeeShop',
+      synchronize: true,
+      logging: true,
+    }),
   ],
   controllers: [],
   providers: [],
