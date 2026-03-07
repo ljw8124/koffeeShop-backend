10b13391263c452b3fd18be3746c3c9027045271
LeeJoungwoo <ljw8124@gmail.com>
2026-03-07 10:29:52 +0900

    이름 변경, service 파일 추가

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
-import { RestaurantsResolver } from './restaurant.resolver';
+import { RestaurantsResolver } from './restaurants.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from './entities/restaurant.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Restaurant])],
   providers: [RestaurantsResolver],
 })
 export class RestaurantsModule {}
