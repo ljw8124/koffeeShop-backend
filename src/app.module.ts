9a15a2dae16ae224d5e543111ed24c12ec28b41f
LeeJoungwoo <ljw8124@gmail.com>
2026-03-11 16:26:19 +0900

    불필요 import 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,14 @@ import { Module } from '@nestjs/common';
 import * as Joi from 'joi';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-// import { join } from 'path';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { Restaurant } from './restaurants/entities/restaurant.entity';
 import { UsersModule } from './users/users.module';
 import { CommonModule } from './common/common.module';
 import * as process from 'node:process';
 import { User } from './users/entities/users.entity';
 
-console.log(Joi);
+// console.log(Joi);
 
 @Module({
   imports: [
