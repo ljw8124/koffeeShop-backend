0164de736163b195348de2b9749fba5493d40dfe
LeeJoungwoo <ljw8124@gmail.com>
2026-03-18 12:46:14 +0900

    interface 활용으로 jwtModule 설정하기

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,7 +28,7 @@ import { JwtModule } from './jwt/jwt.module';
         DB_USERNAME: Joi.string().required(),
         DB_PASSWORD: Joi.string().required(),
         DB_DATABASE: Joi.string().required(),
-        SECRET_KEY: Joi.string().required(),
+        PRIVATE_KEY: Joi.string().required(),
       }),
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
@@ -46,7 +46,9 @@ import { JwtModule } from './jwt/jwt.module';
       logging: process.env.NODE_ENV !== 'prod',
       entities: [User]
     }),
-    JwtModule.forRoot(),
+    JwtModule.forRoot({
+      privateKey: process.env.PRIVATE_KEY,
+    }),
     UsersModule,
     CommonModule,
   ],
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,7 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/users.entity';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
