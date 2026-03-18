2212636644ac548b9b29e45b2896ead89cc97c10
LeeJoungwoo <ljw8124@gmail.com>
2026-03-18 17:23:35 +0900

    Middleware 사용하기

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 /**
  * 이 파일이 유일하게 main.ts 로 import 된다.
  */
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import * as Joi from 'joi';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
@@ -12,13 +17,14 @@ import { CommonModule } from './common/common.module';
 import * as process from 'node:process';
 import { User } from './users/entities/users.entity';
 import { JwtModule } from './jwt/jwt.module';
+import { jwtMiddleware } from './jwt/jwt.middleware';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test",
-      ignoreEnvFile: process.env.NODE_ENV === "prod",
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('dev', 'prod').required(),
         DB_HOST: Joi.string().required(),
@@ -42,7 +48,7 @@ import { JwtModule } from './jwt/jwt.module';
       database: process.env.DB_DATABASE,
       synchronize: process.env.NODE_ENV !== 'prod',
       logging: process.env.NODE_ENV !== 'prod',
-      entities: [User]
+      entities: [User],
     }),
     JwtModule.forRoot({
       privateKey: process.env.PRIVATE_KEY!,
@@ -53,4 +59,22 @@ import { JwtModule } from './jwt/jwt.module';
   controllers: [],
   providers: [],
 })
+
 export class AppModule {}
+/*
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // 특정 경로로만 미들웨어 타는법
+    consumer.apply(jwtMiddleware).forRoutes({
+      path: '/graphql',
+      method: RequestMethod.POST,
+    });
+
+    // 특정경로 제외하는법
+    // consumer.apply(JwtMiddleware).exclude({
+    //   path: 'API',
+    //   method: RequestMethod.ALL,
+    // });
+  }
+}
+*/
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { jwtMiddleware } from './jwt/jwt.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(jwtMiddleware);
   app.useGlobalPipes(
     new ValidationPipe()
   )
