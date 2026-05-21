839e905c86367770de7cd5bf3e1ecf9c8f84d7d7
Joungwoo Lee <ljw8124@gmail.com>
2026-05-21 14:49:36 +0900

    Email 보내는 Public 함수 추가

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -2,39 +2,43 @@ import axios from 'axios';
 import FormData from 'form-data';
 import { Inject, Injectable } from '@nestjs/common';
 import { CONFIG_OPTIONS } from '../common/common.constants';
-import { MailModuleOptions } from './mail.interfaces';
+import { EmailVariable, MailModuleOptions } from './mail.interfaces';
 
 @Injectable()
 export class MailService {
 
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
-  ) {
-    console.log('sendEmail?');
-    this.sendEmail('testing', 'test')
-  }
+  ) {}
 
-  private sendEmail(subject: string, content: string) {
+  private sendEmail(subject: string, template: string, emailVariable: EmailVariable[]) {
     const formData = new FormData();
-    formData.append('from', `Mailgun Sandbox <postmaster@${this.options.domain}>`);
+    formData.append('from', `KoffeeShop <postmaster@${this.options.domain}>`);
     formData.append('to', `ljw8124@gmail.com`);
     formData.append('subject', subject);
-    formData.append('text', content);
+    formData.append('text', template);
     formData.append('template', "koffeeshop");
-    formData.append('v:code', 'assosas');
-    formData.append('v:username', 'ljw8124');
+    emailVariable.forEach(eVar => formData.append(`v:${eVar.key}`, eVar.value));
 
-    const response = axios({
-      method: 'POST',
-      headers: {
-        "Authorization": `Basic ${Buffer.from(`api:${this.options.apiKey}`).toString('base64')}`
-      },
-      url: `https://api.mailgun.net/v3/${this.options.domain}/messages`,
-      data: formData,
-    });
+    try {
+      const response = axios({
+        method: 'POST',
+        headers: {
+          "Authorization": `Basic ${Buffer.from(`api:${this.options.apiKey}`).toString('base64')}`
+        },
+        url: `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+        data: formData,
+      });
+    } catch(e) {
+      console.error(e);
+    }
+  }
 
-    console.log(response);
-
+  sendVerificationEmail(email: string, code: string) {
+    this.sendEmail("Verify Your Email", "verify-email", [
+      {key: code, value: code},
+      {key: 'username', value: email}
+    ]);
   }
 
 }
